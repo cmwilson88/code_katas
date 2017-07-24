7d46a21b51b96681d6fc5155166c145f64cdee8d
Christopher Wilson <cmw.wilson88@gmail.com>
2017-07-24 10:14:55 -0600

    2017-7-24 problems

---
+++ 2017-7-24/problems.js
@@ -0,0 +1,43 @@
+// In this kata, you must create a digital root function.
+
+// A digital root is the recursive sum of all the digits in a number. Given n, take the sum of the digits of n. If that value has two digits, continue reducing in this way until a single-digit number is produced. This is only applicable to the natural numbers.
+
+function digital_root(n) {
+  // ...
+  var answer = n.toString().split("").reduce((a,b) => Number(a) + Number(b));
+  var arr = answer.toString().split("");
+  if(arr.length === 1) {
+    return Number(answer);
+  } else {
+    return digital_root(answer)
+  }
+}
+
+// This time we want to write calculations using functions and get the results. Let's have a look at some examples:
+
+// seven(times(five())); // must return 35
+// four(plus(nine())); // must return 13
+// eight(minus(three())); // must return 5
+// six(dividedBy(two())); // must return 3
+// Requirements:
+
+// There must be a function for each number from 0 ("zero") to 9 ("nine")
+// There must be a function for each of the following mathematical operations: plus, minus, times, dividedBy (divided_by in Ruby)
+// Each calculation consist of exactly one operation and two numbers
+// The most outer function represents the left operand, the most inner function represents the right operand
+
+function zero(fn) { return fn ? fn(0) : 0}
+function one(fn) {return fn ? fn(1) : 1}
+function two(fn) {return fn ? fn(2) : 2}
+function three(fn) {return fn ? fn(3) : 3}
+function four(fn) {return fn ? fn(4) : 4}
+function five(fn) {return fn ? fn(5) : 5}
+function six(fn) {return fn ? fn(6) : 6}
+function seven(fn) {return fn ? fn(7) : 7}
+function eight(fn) {return fn ? fn(8) : 8}
+function nine(fn) {return fn ? fn(9) : 9}
+
+function plus(b) { return a => a + b}
+function minus(b) {return  a => a-b }
+function times(b) { return a => a * b}
+function dividedBy(b) { return a => a/b}
