530f17a1f46c503618e2f0156f5a026a0202ab0d
Christopher Wilson <cmw.wilson88@gmail.com>
2017-07-25 10:22:37 -0600

    2017-7-25 problems

--- 2017-7-24/problems.js
+++ 2017-7-24/problems.js
@@ -41,3 +41,71 @@ function plus(b) { return a => a + b}
 function minus(b) {return  a => a-b }
 function times(b) { return a => a * b}
 function dividedBy(b) { return a => a/b}
+
+
+////////////////////////////////////////
+// There is an array of non-negative integers. A second array is formed by
+// shuffling the elements of the first array and deleting a random element.
+// Given these two arrays, find which element is missing in the second array.
+
+let arrOne = ['Bob Ross', 'Tim Allen', 'Richard Simmons', 'big bird'];
+let arrTwo = ['Bob Ross', 'Richard Simmons', 'Tim Allen'];
+
+function findMissing(arr1, arr2) {
+  for(var i = 0; i < arr1.length; i++) {
+    if (!arr2.includes(arr1[i])){
+      return arr1[i]
+    }
+  }
+}
+
+findMissing(arrOne, arrTwo)
+
+// Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+
+// Examples:
+
+// spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+// spinWords( "This is a test") => returns "This is a test" 
+// spinWords( "This is another test" )=> returns "This is rehtona test"
+
+
+function spinWords(str){
+  //TODO Have fun 
+  return str.split(" ").map(word => word.length >= 5 ? word.split("").reverse().join("") : word).join(" ")
+  
+}
+
+spinWords('Hey fellow warriors')
+
+// What is an anagram? Well, two words are anagrams of each other if they both contain the same letters. For example:
+
+// 'abba' & 'baab' == true
+
+// 'abba' & 'bbaa' == true
+
+// 'abba' & 'abbba' == false
+// Write a function that will find all the anagrams of a word from a list. You will be given two inputs a word and an array with words. You should return an array of all the anagrams or an empty array if there are none. For example:
+
+// anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
+
+anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) //=> ['carer', 'racer']
+
+//anagrams('laser', ['lazing', 'lazy',  'lacer'])
+
+
+function anagrams(word, words) {
+  let indices = [];
+  let results = [];
+  let compare = word.split("").sort().join("");
+  words.map(word => word.split("").sort().join("")).forEach((word, i) => word === compare ? indices.push(i) : null)
+  words.forEach((word, index) => {
+    indices.forEach(item => {
+      index === item ? results.push(word) : null
+    })
+  })
+  return results
+}
+
+
