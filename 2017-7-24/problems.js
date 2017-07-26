0fabef6e2d111ca7666aa16667e665f586c2436c
Christopher Wilson <cmw.wilson88@gmail.com>
2017-07-26 09:36:41 -0600

    2017-7-26 Problems

--- 2017-7-24/problems.js
+++ 2017-7-24/problems.js
@@ -109,3 +109,30 @@ function anagrams(word, words) {
 }
 
 
+///////////////////////////////////////////////////////////////
+// My friend wants a new band name for her band. She like bands that use the formula: 'The' + a noun with first letter capitalized.
+
+// dolphin -> The Dolphin
+
+// However, when a noun STARTS and ENDS with the same letter, she likes to repeat the noun twice and connect them together with the first and last letter, combined into one word like so (WITHOUT a 'The' in front):
+
+// alaska -> Alaskalaska
+
+// europe -> Europeurope
+
+// Can you write a function that takes in a noun as a string, and returns her preferred band name written as a string?
+
+function bandNameGenerator(str) {
+  // Happy coding
+  if (str.charAt(0) === str.charAt(str.length-1)) {
+    let newName = str + str.slice(1)
+    return newName.charAt(0).toUpperCase() + newName.slice(1)
+  } else {
+    return 'The ' + str.charAt(0).toUpperCase() + str.slice(1)
+  }
+}
+
+bandNameGenerator('tart')//, 'Tartart')
+bandNameGenerator('dolphin')
+
+
