8ca0288b4e806a77f424639d7e456a6028bec594
Christopher Wilson <cmw.wilson88@gmail.com>
2017-09-17 20:18:44 -0600

    9/17/17 problems

--- 2017-9-11/problems.js
+++ 2017-9-11/problems.js
@@ -11,3 +11,17 @@ class SmallestIntegerFinder {
      return args.sort((a,b) => a - b)[0]
   }
 }
+
+
+// Write simple .camelCase method (camel_case function in PHP or CamelCase in C#) for strings. 
+// All words must have their first letter capitalized without spaces.
+
+// For instance:
+
+// "hello case".camelCase() => HelloCase
+// "camel case word".camelCase() => CamelCaseWord
+
+String.prototype.camelCase=function(){
+  //your code here
+  return this.split(' ').map(item => item.charAt(0).toUpperCase() + item.slice(1)).join('')
+}
