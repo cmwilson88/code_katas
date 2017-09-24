aad1dd5186c09afa0358bd74460a0715e0765995
Christopher Wilson <cmw.wilson88@gmail.com>
2017-09-24 17:57:48 -0400

    third kata for today

--- 2017-9-18/problems.js
+++ 2017-9-18/problems.js
@@ -393,3 +393,51 @@ function newsIsFalse(news) {
   }
   return compare.length ? 'NO' : 'YES'
 }
+//////////////////////////////////////////////////////////////////////
+// If we were to set up a Tic-Tac-Toe game, we would want to know whether the board's current state is solved, wouldn't we? Our goal is to create a function that will check that for us!
+
+// Assume that the board comes in the form of a 3x3 array, where the value is 0 if a spot is empty, 1 if it is an X, or 2 if it is an O, like so:
+
+// [[0,0,1],
+//  [0,1,2],
+//  [2,1,0]]
+// We want our function to return -1 if the board is not solved yet, 1 if X won, 2 if O won, or 0 if it's a cat's game (i.e. a draw).
+
+// You may assume that the board passed in is valid in the context of a game of Tic-Tac-Toe.
+
+function isSolved(board) {
+  // TODO: Check if the board is solved!
+  let isFinished = false;
+  let combos = [
+      board[0],
+      board[1],
+      board[2],
+      [board[0][0], board[1][1], board[2][2]],
+      [board[0][2], board[1][1], board[2][0]],
+      [board[0][0], board[1][0], board[2][0]],
+      [board[0][1], board[1][1], board[2][1]],
+      [board[0][2], board[1][2], board[2][2]]
+    ];
+  let totals = [];
+  let winner = 0;
+  
+  combos = combos.filter(item => !item.includes(0))
+  if(combos.length === 8) {
+    isFinished = true;
+  }
+  combos.forEach(item => totals.push(item.reduce((a,b) => a+b,0)));
+  
+    if (totals.includes(3)) {
+      winner = 1
+    } else if (totals.includes(6)) {
+      winner = 2
+    } else {
+      if (isFinished) {
+        winner = 0
+      } else {
+        winner = -1
+      }
+    }
+  return winner
+  
+}
