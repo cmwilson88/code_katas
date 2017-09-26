bcfc4170794fce82d3dd1d34a882826f38b839da
Christopher Wilson <cmw.wilson88@gmail.com>
2017-09-26 18:15:41 -0400

    Tuesday katas. 3 blues

--- 2017-9-25/problems.js
+++ 2017-9-25/problems.js
@@ -80,3 +80,250 @@ function insertDash(num) {
 }
 
 insertDash(454793)
+////////////////////////////////////////////////////////
+
+// Create a function taking a positive integer as its parameter and returning a string containing the Roman Numeral representation of that integer.
+
+// Modern Roman numerals are written by expressing each digit separately starting with the left most digit and skipping any digit with a value of zero. In Roman numerals 1990 is rendered: 1000=M, 900=CM, 90=XC; resulting in MCMXC. 2008 is written as 2000=MM, 8=VIII; or MMVIII. 1666 uses each Roman symbol in descending order: MDCLXVI.
+
+// Example:
+
+// solution(1000); // should return 'M'
+// Help:
+
+// Symbol    Value
+// I          1
+// V          5
+// X          10
+// L          50
+// C          100
+// D          500
+// M          1,000
+// Remember that there can't be more than 3 identical symbols in a row.
+
+function solution(number){
+  // convert the number to a roman numeral
+  const roman = {
+    'M': 1000,
+    'CM': 900,
+    'D': 500,
+    'CD': 400,
+    'C': 100,
+    'XC': 90,
+    'L': 50,
+    'XL': 40,
+    'X': 10,
+    'IX': 9,
+    'V': 5,
+    'IV': 4,
+    'I': 1
+  }
+  let output = ''
+  for(let i in roman) {
+    while(number >= roman[i]) {
+      output += i;
+      number -= roman[i]
+    }
+  }
+  return output
+}
+////////////////////////////////////////////////////////
+
+// Create a function that takes a Roman numeral as its argument and returns its value as a numeric decimal integer. You don't need to validate the form of the Roman numeral.
+// Modern Roman numerals are written by expressing each decimal digit of the number to be encoded separately, starting with the leftmost digit and skipping any 0s. So 1990 is rendered "MCMXC" (1000 = M, 900 = CM, 90 = XC) and 2008 is rendered "MMVIII" (2000 = MM, 8 = VIII). The Roman numeral for 1666, "MDCLXVI", uses each letter in descending order.
+
+// Example:
+// solution('XXI'); // should return 21
+
+function solution(roman) {
+  roman = roman.split('')
+  let number = 0
+  for (let i = 0; i < roman.length; i++) {
+    switch(roman[i]) {
+      case 'M':
+        number += 1000
+        break;
+      case 'D': 
+        number += 500
+        break;
+      case 'C':
+        if(roman[i+1] === 'M') {
+          number += 900;
+          i++
+          break;
+        } else if (roman[i+1] === 'D') {
+          number += 400;
+          i++
+          break;
+        } else {
+          number += 100;
+          break;
+        }
+      case 'L':
+        number += 50
+        break;
+      case 'X':
+        if(roman[i+1] === 'C') {
+          number += 90;
+          i++
+          break;
+        } else if (roman[i+1] === 'L') {
+          number += 40;
+          i++;
+          break;
+        } else {
+          number += 10
+          break;          
+        }
+      case 'V':
+        number += 5
+        break;
+      case 'I':
+        if(roman[i+1] === 'X') {
+          number += 9;
+          i++
+          break;
+        } else if (roman[i+1] === 'V') {
+          number += 4;
+          i++
+          break;
+        } else {
+          number += 1
+          break;          
+        }
+      default:
+        break;
+    }
+  }
+  return number
+}
+//////////////////////////////////////////////////////////
+
+// Sudoku Background
+
+// Sudoku is a game played on a 9x9 grid. The goal of the game is to fill all cells of the grid with digits from 1 to 9, so that each column, each row, and each of the nine 3x3 sub-grids (also known as blocks) contain all of the digits from 1 to 9. 
+// (More info at: http://en.wikipedia.org/wiki/Sudoku)
+
+// Sudoku Solution Validator
+// Write a function validSolution/ValidateSolution/valid_solution() that accepts a 2D array representing a Sudoku board, and returns true if it is a valid solution, or false otherwise. The cells of the sudoku board may also contain 0's, which will represent empty cells. Boards containing one or more zeroes are considered to be invalid solutions.
+// The board is always 9 cells by 9 cells, and every cell only contains integers from 0 to 9.
+
+// Examples
+// validSolution([
+//   [5, 3, 4, 6, 7, 8, 9, 1, 2],
+//   [6, 7, 2, 1, 9, 5, 3, 4, 8],
+//   [1, 9, 8, 3, 4, 2, 5, 6, 7],
+//   [8, 5, 9, 7, 6, 1, 4, 2, 3],
+//   [4, 2, 6, 8, 5, 3, 7, 9, 1],
+//   [7, 1, 3, 9, 2, 4, 8, 5, 6],
+//   [9, 6, 1, 5, 3, 7, 2, 8, 4],
+//   [2, 8, 7, 4, 1, 9, 6, 3, 5],
+//   [3, 4, 5, 2, 8, 6, 1, 7, 9]
+// ]); // => true
+// validSolution([
+//   [5, 3, 4, 6, 7, 8, 9, 1, 2], 
+//   [6, 7, 2, 1, 9, 0, 3, 4, 8],
+//   [1, 0, 0, 3, 4, 2, 5, 6, 0],
+//   [8, 5, 9, 7, 6, 1, 0, 2, 0],
+//   [4, 2, 6, 8, 5, 3, 7, 9, 1],
+//   [7, 1, 3, 9, 2, 4, 8, 5, 6],
+//   [9, 0, 1, 5, 3, 7, 2, 1, 4],
+//   [2, 8, 7, 4, 1, 9, 6, 3, 5],
+//   [3, 0, 0, 4, 8, 1, 1, 7, 9]
+// ]); // => false
+
+function validSolution(board) {
+  
+  // checkColumns(board)  
+  if(
+    // !checkRows(board) &&
+    // !checkColumns(board) &&
+    !checkBlocks(board)
+    ) {
+    return false
+  } else {
+    return true
+  }
+  
+  function checkDigits(array) {
+    for(let i = 0, j=array.length; i<j; i++) {
+      if(array[i] < 1) {
+        return false
+      }
+      if(array.indexOf(array[i]) !== array.lastIndexOf(array[i])) {
+        return false
+      }
+    }
+    return true
+  }
+  
+  function checkRows(board) {
+    for(let i = 0, j=board.length; i<j; i++) {
+      if(!checkDigits(board[i])) {
+        return false
+      }
+    }
+    return true
+  }
+  
+  function checkColumns(board) {
+    let tempArray = [];
+    for(let i=0, j=board.length; i<j; i++) {
+      for(let k=0; k<j; k++) {
+        tempArray[k] = board[k][i];
+      }
+      if(!checkDigits(tempArray)) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  function checkBlock(board, startCol, startRow) {
+    let tempRegion = []
+    let index = 0;
+    for(let row = startRow; row < startRow+3; row++) {
+      for(let col = startCol; col < startCol+3; col++) {
+        tempRegion[index] = board[row][col];
+        index++
+      }
+    }
+    console.log(tempRegion)
+    return checkDigits(tempRegion)
+  }
+  
+  function checkBlocks(board) {
+    for(let col = 0; col < board.length; col+=3) {
+      for(let row = 0; row < board.length; row+=3) {
+        if(!checkBlock(board, col, row)) {
+          return false;
+        } 
+      }
+    }
+    return true;
+  }
+}
+/////////////////////////////////////////////////////
+// An array is defined to be odd-heavy if it contains at least one odd element and every element whose value is odd is greater than every even-valued element.
+
+// eg.
+
+// Array [11,4,9,2,8] is odd-heavy 
+// because:- its odd elements [11,9] are greater than all the even elements [4,2,8]
+
+// Array [11,4,9,2,3,10] is not odd-heavy
+// because:- one of it's even element 10 from [4,2,10] is greater than two of its odd elements [9,3] from [ 11,9,3]
+// write a function called isOddHeavy that accepts an integer array and returns true if the array is odd-heavy else return false.
+function isOddHeavy(n){
+  if(!n.some(item => item % 2 !==0)) {
+    return false
+  }
+  let odd = n.filter(item => item % 2 !==0).sort((a,b) => a - b)
+  let even = n.filter(item => item % 2 ===0)
+  for (let i = 0; i < even.length; i++) {
+    if(even[i] > odd[0]) {
+      return false
+    }
+  }
+  return true
+}
