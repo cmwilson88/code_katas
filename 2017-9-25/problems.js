6fdff8f1a5d431aa0aae7bbf675109a4a3a94820
Christopher Wilson <cmw.wilson88@gmail.com>
2017-09-29 08:43:36 -0400

    thursday problems

--- 2017-9-25/problems.js
+++ 2017-9-25/problems.js
@@ -327,3 +327,158 @@ function isOddHeavy(n){
   }
   return true
 }
+///////////////////////////////////////////////////////
+// Write a class called User that is used to calculate the amount that a user will progress through a ranking system similar to the one Codewars uses.
+
+// Business Rules:
+
+// A user starts at rank -8 and can progress all the way to 8.
+// There is no 0 (zero) rank. The next rank after -1 is 1.
+// Users will complete activities. These activities also have ranks.
+// Each time the user completes a ranked activity the users rank progress is updated based off of the activity's rank
+// The progress earned from the completed activity is relative to what the user's current rank is compared to the rank of the activity
+// A user's rank progress starts off at zero, each time the progress reaches 100 the user's rank is upgraded to the next level
+// Any remaining progress earned while in the previous rank will be applied towards the next rank's progress (we don't throw any progress away). The exception is if there is no other rank left to progress towards (Once you reach rank 8 there is no more progression).
+// A user cannot progress beyond rank 8.
+// The only acceptable range of rank values is -8,-7,-6,-5,-4,-3,-2,-1,1,2,3,4,5,6,7,8. Any other value should raise an error.
+// The progress is scored like so:
+
+// Completing an activity that is ranked the same as that of the user's will be worth 3 points
+// Completing an activity that is ranked one ranking lower than the user's will be worth 1 point
+// Any activities completed that are ranking 2 levels or more lower than the user's ranking will be ignored
+// Completing an activity ranked higher than the current user's rank will accelerate the rank progression. The greater the difference between rankings the more the progression will be increased. The formula is 10 * d * d where d equals the difference in ranking between the activity and the user.
+// Logic Examples:
+
+// If a user ranked -8 completes an activity ranked -7 they will receive 10 progress
+// If a user ranked -8 completes an activity ranked -6 they will receive 40 progress
+// If a user ranked -8 completes an activity ranked -5 they will receive 90 progress
+// If a user ranked -8 completes an activity ranked -4 they will receive 160 progress, resulting in the user being upgraded to rank -7 and having earned 60 progress towards their next rank
+// If a user ranked -1 completes an activity ranked 1 they will receive 10 progress (remember, zero rank is ignored)
+// Code Usage Examples:
+
+// var user = new User()
+// user.rank // => -8
+// user.progress // => 0
+// user.incProgress(-7)
+// user.progress // => 10
+// user.incProgress(-5) // will add 90 progress
+// user.progress # => 0 // progress is now zero
+// user.rank # => -7 // rank was upgraded to -7
+// Note: Codewars no longer uses this algorithm for its own ranking system. It uses a pure Math based solution that gives consistent results no matter what order a set of ranked activities are completed at.
+
+function User() {
+  let rank = -8;
+  let progress = 0;
+  
+  function incProgress(actLevel) {
+    if(actLevel >= -8 && actLevel <= 8 && actLevel) {
+      if(this.rank === 8) {
+        this.progress = 0;
+      } else if(actLevel === -1 && this.rank === 1) {
+        this.progress += 1
+      } else if(actLevel === this.rank - 1) {
+        this.progress += 1
+      } else if (actLevel === this.rank) {
+        this.progress += 3
+      } else if (actLevel > this.rank) {
+        if(actLevel > 0 && this.rank < 0) {
+          let diff = actLevel - this.rank - 1
+          this.progress += 10 * diff * diff  
+        } else {
+          let diff = actLevel - this.rank
+          this.progress += 10 * diff * diff
+        }
+      }
+      
+      if(this.progress >= 100 && this.rank < 8) {
+        let rankLevels = Math.floor(this.progress / 100)
+        this.progress = this.progress % 100
+        for(let i = 1; i <= rankLevels; i++) {
+          if(this.rank === -1) {
+            this.rank+=2
+          } else {
+            this.rank++
+            if(this.rank === 8) {
+              this.progress = 0;
+            }
+          }  
+        }
+      }      
+    } else{
+      throw new Error(`Activity level can't be zero`);
+    }
+
+  }
+  
+  return {
+    rank,
+    progress,
+    incProgress
+  }
+}
+///////////////////////////////////////////////////////////////////////
+// You need to create a function that will validate if given parameters are valid geographical coordinates.
+
+// Valid coordinates look like the following: "23.32353342, -32.543534534". The return value should be either true or false.
+
+// Latitude (which is first float) can be between 0 and 90, positive or negative. Longitude (which is second float) can be between 0 and 180, positive or negative.
+
+// Coordinates can only contain digits, or one of the following symbols (including space after comma) -, .
+
+// There should be no space between the minus "-" sign and the digit after it.
+
+// Here are some valid coordinates:
+
+// -23, 25
+// 24.53525235, 23.45235
+// 04, -23.234235
+// 43.91343345, 143
+// 4, -3
+// And some invalid ones:
+
+// 23.234, - 23.4234
+// 2342.43536, 34.324236
+// N23.43345, E32.6457
+// 99.234, 12.324
+// 6.325624, 43.34345.345
+// 0, 1,2
+// 0.342q0832, 1.2324
+
+function isValidCoordinates(coordinates){
+  if((/[^0-9- .,]+/g).test(coordinates)) {
+    return false
+  } 
+  let arr = coordinates.split(',')
+  if(arr.length > 2) {
+    return false
+  }
+  if(arr[0] > 90 || arr[0] < -90) {
+    return false
+  }
+  if(arr[1] > 180 || arr[1] < -180) {
+    return false
+  }
+  for(let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].trim();
+    if(arr[i].includes(' ')) {
+      return false
+    }
+    let dashCount = 0;
+    let dotCount = 0;
+    arr[i].split('').forEach(char => {
+      if(char === '.') {
+        dotCount++
+      } else if (char === '-') {
+        if(arr[i] > 1) {
+          return false
+        }
+        dashCount++
+      }
+    })
+    
+    if(dashCount > 1 || dotCount > 1) {
+      return false
+    }
+  }
+  return true
+}
